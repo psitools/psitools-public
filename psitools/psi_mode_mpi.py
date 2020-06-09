83a31ecb339bdc2c415b3e5d0a410b8d1cdf276c
Colin McNally <colin@colinmcnally.ca>
2020-06-09 13:36:44 +0100

    psi_mode_mpi: Make the output less verbose

--- psitools/psi_mode_mpi.py
+++ psitools/psi_mode_mpi.py
@@ -41,7 +41,9 @@ class MpiScheduler:
            assignments, do not work.
         """
 
-        print('Master Process rank ', self.rank, flush=True)
+        if self.verbose:
+            print('Master Process rank ', self.rank, flush=True)
+        emptyflag = False
         am = collections.deque(range(0, len(campaign)))
         finishedruns = {}
         waiting = []
@@ -84,9 +86,11 @@ class MpiScheduler:
                     print('Rank {:d} commanded to wait'.format(dest), flush=True)
                 self.comm.send(['wait'], dest=dest, tag=1)
 
-            if (len(am) < 10):
+            if (len(am) < 10 and not emptyflag):
                 print('***Master unassigned list is now ',
                       list(am), flush=True)
+                if len(am) == 0:
+                    emptyflag = True
             else:
                 if self.verbose:
                     print('***Master unassigned list has {:d} entries.\n'
@@ -99,14 +103,16 @@ class MpiScheduler:
         for dest in range(1, self.nranks):
             # we never track this, just let it die
             self.comm.isend(['exit'], dest=dest, tag=1)
-        print('Exit rank ', self.rank, flush=True)
+        if self.verbose:
+            print('Exit rank ', self.rank, flush=True)
         return finishedruns
 
     def slaveprocess(self, campaign):
         """Execution of a MPI slave process, waits for commands from master,
            executes chunks of work from campaign
         """
-        print('Slave Process rank ', self.rank, flush=True)
+        if self.verbose:
+            print('Slave Process rank ', self.rank, flush=True)
 
         # cache the object, need here to know none have been generated
         self.PSIModeArgs = {}
@@ -136,7 +142,8 @@ class MpiScheduler:
             else:
                 request = self.comm.isend(['waiting'], dest=0, tag=2)
 
-        print('Exit rank ', self.rank, flush=True)
+        if self.verbose:
+            print('Exit rank ', self.rank, flush=True)
 
     def runcompute(self, args):
         if 'random_seed' in args:
