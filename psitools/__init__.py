7d5f472cd0e822b0d81cf96f02804ebebe374068
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2021-09-30 13:36:39 +0100

    grid_refine

--- psitools/__init__.py
+++ psitools/__init__.py
@@ -24,7 +24,7 @@ from . import monodisperse_si
 from . import power_bump
 from . import psi_mode
 #from . import psi_mode_mpi
-from . import psi_grid_refine
+#from . import psi_grid_refine
 from . import tanhsinh
 from . import taus_gridding
 from . import terminalvelocitysolver
