de57d0481d70f0b6c19fe15c630990e0f89a3492
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2022-04-26 13:19:28 +0100

    Support for monodisperse

--- psitools/sizedensity.py
+++ psitools/sizedensity.py
@@ -34,15 +34,32 @@ class SizeDensity():
         self.amin = size_range[0]
         self.amax = size_range[1]
 
-        if sigma_integral is None:
-            self.rhod = integrate.quad(sigma, self.amin, self.amax)[0]
+        if callable(sigma):
+            if sigma_integral is None:
+                self.rhod = integrate.quad(sigma, self.amin, self.amax)[0]
+            else:
+                self.rhod = sigma_integral
+            self.f = lambda x: self.amax*sigma(self.amax*x)/self.rhod
+            self.monodisperse = False
         else:
-            self.rhod = sigma_integral
-        self.f = lambda x: self.amax*sigma(self.amax*x)/self.rhod
-
+            print('Monodisperse size distribution')
+            self.rhod = sigma
+            self.f = lambda x: self.amax
+            self.monodisperse = True
         # Empty list of poles
         self.poles = []
 
     def __call__(self, x):
         """Return amax*sigma(amax*x)/rho_d"""
         return self.f(x)
+
+    def sigma(self, x):
+        return self.f(x/self.amax)/self.amax
+
+    def integrate(self, func):
+        """Integrate sigma*func over all sizes"""
+        if self.monodisperse is False:
+            g = lambda x: self.f(x)*func(self.amax*x)
+            return integrate.fixed_quad(g, self.amin/self.amax, 1, n=50)[0]
+        else:
+            return func(self.amax)
